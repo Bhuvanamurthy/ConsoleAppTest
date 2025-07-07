241f7e80bb661878acc51cf75051516b87f401c1
Bhuvanamurthy <murthybhuvana02@gmail.com>
2025-07-07 15:34:03 +0530

    class1 completed

--- ConsoleAppGit/Class1.cs
+++ ConsoleAppGit/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace ConsoleAppGit
 {
     internal class Class1
     {
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+        
     }
 }
